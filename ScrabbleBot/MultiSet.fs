2e60abb114d0d3aa17dd29aff7c5e378ebb8539d
Daniel Millard <dmil@itu.dk>
2024-04-24 11:05:09 +0200

    remove old comments

--- ScrabbleBot/MultiSet.fs
+++ ScrabbleBot/MultiSet.fs
@@ -1,13 +1,6 @@
 ﻿// Insert your MultiSet.fs file here. All modules must be internal
 
 module internal MultiSet
-    
-    // ORIGINAL FILE CONTENT:
-    //type MultiSet<'a> = Temp of unit // Not implemented
-    //let empty : MultiSet<'a> = Temp () // Not implemented
-    //let add   : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> = fun _ _ _ -> failwith "Not implemented"
-    //let fold  : ('b -> 'a -> uint32 -> 'b) -> 'b -> MultiSet<'a> -> 'b = fun _ _ _ -> failwith "Not implemented"
-
     type MultiSet<'a when 'a : comparison> = R of Map<'a, uint32>
 
     let empty = R Map.empty
